485cba1d19634a2b27e9df5367339c8e25bc3d24
dnhem27 <seoungdavid.nhem@gmail.com>
2021-05-20 01:58:42 -0400

    Initialize Inventory MS Controller with required endpoints

--- inventory/src/main/java/com/revature/shop/inventory/RevShopApplication.java
+++ inventory/src/main/java/com/revature/shop/inventory/RevShopApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RevShopApplication {
+public class RevShopApplication
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(RevShopApplication.class, args);
     }
 
