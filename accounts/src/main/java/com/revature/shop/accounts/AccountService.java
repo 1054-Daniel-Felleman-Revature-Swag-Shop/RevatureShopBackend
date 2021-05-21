3d78570a95f405205393920658fb0454ac69c5aa
Dante Pizzini <pizzini@wisc.edu>
2021-05-21 15:44:21 -0600

    local stash-accounts-hystrix

--- accounts/src/main/java/com/revature/shop/accounts/AccountService.java
+++ accounts/src/main/java/com/revature/shop/accounts/AccountService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+
 import java.util.Optional;
 
 @Service
@@ -18,6 +19,7 @@ public class AccountService {
     }
 
     @Transactional
+
     public boolean modPoints(int userId, PointChange change) {
         Optional<Account> optional = repo.findById(userId);
 
--- accounts/src/main/java/com/revature/shop/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/revature/shop/accounts/AccountsApplication.java
@@ -2,8 +2,13 @@ package com.revature.shop.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
 
 @SpringBootApplication
+@EnableHystrix
+@EnableHystrixDashboard
 public class AccountsApplication {
 
     public static void main(String[] args) {
