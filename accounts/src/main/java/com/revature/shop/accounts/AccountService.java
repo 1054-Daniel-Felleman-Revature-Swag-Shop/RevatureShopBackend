6509ed037e5c473a731f486671f2368eee245257
Dante Pizzini <pizzini@wisc.edu>
2021-05-22 09:48:16 -0600

    Eureka server is launching, hitting bugs trying to map AccountApplication as a client of the Eureka server

--- accounts/src/main/java/com/revature/shop/accounts/AccountService.java
+++ accounts/src/main/java/com/revature/shop/accounts/AccountService.java
@@ -1,5 +1,6 @@
 package com.revature.shop.accounts;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,7 +20,7 @@ public class AccountService {
     }
 
     @Transactional
-
+    @HystrixCommand(fallbackMethod = "downService")
     public boolean modPoints(int userId, PointChange change) {
         Optional<Account> optional = repo.findById(userId);
 
@@ -38,5 +39,9 @@ public class AccountService {
         pointsRepo.save(change);
 
         return true;
+    }
+
+    public String downService(){
+        return "The Accounts service is currently under maintenance, please check in later";
     }
 }
