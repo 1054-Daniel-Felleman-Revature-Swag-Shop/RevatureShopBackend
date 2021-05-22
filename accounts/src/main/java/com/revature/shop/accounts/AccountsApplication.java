add51c2f0d0eae9810ea6d307e9d730317451cd5
Dante Pizzini <pizzini@wisc.edu>
2021-05-22 11:14:05 -0600

    Eureka server launching and attempting to monitor for any eureka clients; Failing to map AccountsApplication as a client discoverable by the eureka server; Potentially look into Consul and Zuul as an alternative;

--- accounts/src/main/java/com/revature/shop/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/revature/shop/accounts/AccountsApplication.java
@@ -2,11 +2,11 @@ package com.revature.shop.accounts;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
@@ -22,8 +22,7 @@ import java.util.List;
 @EnableHystrix
 @EnableHystrixDashboard
 @EnableTurbine
-@EnableDiscoveryClient
-
+@EnableEurekaClient
 public class AccountsApplication {
 
     public static void main(String[] args) {
