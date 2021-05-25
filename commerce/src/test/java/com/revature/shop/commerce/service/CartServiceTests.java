b9706360e45caf7dca7c04b19ab8568f0d1f4623
Abdul Moeed <abdul.moeed.ak@gmail.com>
2021-05-25 13:30:50 -0500

    testing implemented successfully

--- commerce/src/test/java/com/revature/shop/commerce/service/CartServiceTests.java
+++ commerce/src/test/java/com/revature/shop/commerce/service/CartServiceTests.java
@@ -1,16 +1,23 @@
 package com.revature.shop.commerce.service;
 
-import com.revature.shop.commerce.model.*;
-import com.revature.shop.commerce.dto.*;
-import com.revature.shop.commerce.exception.*;
-import com.revature.shop.commerce.repository.*;
+import com.revature.shop.commerce.dto.CartDto;
+import com.revature.shop.commerce.dto.StockItemDto;
+import com.revature.shop.commerce.exception.ItemNotInCartException;
+import com.revature.shop.commerce.exception.ItemOutOfStockException;
 
-
+import com.revature.shop.commerce.model.Cart;
+import com.revature.shop.commerce.model.StockItem;
+import com.revature.shop.commerce.repository.CartRepository;
+import com.revature.shop.commerce.repository.StockItemRepository;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
 
 
 import java.util.HashMap;
@@ -20,40 +27,51 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
-public class CartServiceUnitTests {
+public class CartServiceTests {
+
     @InjectMocks
     com.revature.shop.commerce.service.CartService mockedCartService;
 
     @Mock
     CartRepository cartRepository;
 
-    @Mock
-    StockItemRepository stockItemRepository;
+    static RestTemplate restTemplate = new RestTemplate();
+
+    static StockItem stockItem = new StockItem(99998,"test-cup", 10, 10);
+    static StockItem stockItem2 = new StockItem(99999,"test-t-shirt", 15, 20);
+
+    @BeforeClass
+    public static void addTestItem () {
+        restTemplate.put("http://localhost:9001/inventoryms/api/inventory/stockitem/new", stockItem);
+        restTemplate.put("http://localhost:9001/inventoryms/api/inventory/stockitem/new", stockItem2);
+    }
+
+    @AfterClass
+    public static void deleteTestItem () {
+        restTemplate.delete("http://localhost:9001/inventoryms/api/inventory/delete/item/name?itemName="+stockItem.getItemName());
+        restTemplate.delete("http://localhost:9001/inventoryms/api/inventory/delete/item/name?itemName="+stockItem2.getItemName());
+    }
 
     @Test
     public void updateCart() throws ItemOutOfStockException {
         Cart cart = new Cart(1, "abdulmoeedak", new HashMap<String, Integer>(){{
-            put("t-shirt",1);
+            put("test-t-shirt",1);
         }});
-        StockItem stockItem = new StockItem(1, "cup", 10, 10);
-        StockItem stockItem2 = new StockItem(1, "t-shirt", 15, 20);
-        when(stockItemRepository.findByItemName("cup")).thenReturn(stockItem);
-        when(stockItemRepository.findByItemName("t-shirt")).thenReturn(stockItem2);
         when(cartRepository.findOneByMyShopper("abdulmoeedak")).thenReturn(cart);
-        StockItemDto stockItemDto = new StockItemDto("abdulmoeedak", "cup", 10, 1);
+        StockItemDto stockItemDto = new StockItemDto("abdulmoeedak", "test-cup", 10, 1);
 
         CartDto cartDto = mockedCartService.updateCart(stockItemDto);
         assertEquals(2, cartDto.getStockItemDtoList().size());
         cart = new Cart(1, "abdulmoeedak", new HashMap<String, Integer>(){{
-            put("t-shirt",1);
-            put("cup",1);
+            put("test-t-shirt",1);
+            put("test-cup",1);
         }});
         when(cartRepository.findOneByMyShopper("abdulmoeedak")).thenReturn(cart);
         cartDto = mockedCartService.updateCart(stockItemDto);
         assertEquals(2, cartDto.getStockItemDtoList().size());
-        assertTrue(cartDto.getStockItemDtoList().stream().anyMatch(stDto -> stDto.getItemName().equals("cup") && stDto.getCartQuantity() == 2));
-        stockItem = new StockItem(1, "cup", 10, 0);
-        when(stockItemRepository.findByItemName("cup")).thenReturn(stockItem);
+        assertTrue(cartDto.getStockItemDtoList().stream().anyMatch(stDto -> stDto.getItemName().equals("test-cup") && stDto.getCartQuantity() == 2));
+        stockItem.setQuantity(0);
+        restTemplate.put("http://localhost:9001/inventoryms/api/inventory/stockitem/update/quantity", stockItem);
         Exception exception = assertThrows(ItemOutOfStockException.class, () -> mockedCartService.updateCart(stockItemDto));
         assertTrue(exception.getMessage().equals("Item Out Of Stock"));
     }
@@ -61,30 +79,27 @@ public class CartServiceUnitTests {
     @Test
     public void removeItemFromCart () throws ItemNotInCartException {
         Cart cart = new Cart(1, "abdulmoeedak", new HashMap<String, Integer>(){{
-            put("t-shirt",1);
-            put("cup",1);
+            put("test-t-shirt",1);
+            put("test-cup",1);
         }});
-        StockItemDto stockItemDto = new StockItemDto("abdulmoeedak", "t-shirt", 10, 1);
-        StockItem stockItem = new StockItem(1, "cup", 10, 10);
-        StockItem stockItem2 = new StockItem(1, "t-shirt", 15, 20);
-        when(stockItemRepository.findByItemName("cup")).thenReturn(stockItem);
-        when(stockItemRepository.findByItemName("t-shirt")).thenReturn(stockItem2);
+        StockItemDto stockItemDto = new StockItemDto("abdulmoeedak", "test-t-shirt", 10, 1);
         when(cartRepository.findOneByMyShopper("abdulmoeedak")).thenReturn(cart);
         CartDto cartDto = mockedCartService.removeItemFromCart(stockItemDto);
         assertEquals(1, cartDto.getStockItemDtoList().size());
         cart = new Cart(1, "abdulmoeedak", new HashMap<String, Integer>(){{
-            put("t-shirt",2);
-            put("cup",1);
+            put("test-t-shirt",2);
+            put("test-cup",1);
         }});
         when(cartRepository.findOneByMyShopper("abdulmoeedak")).thenReturn(cart);
         cartDto = mockedCartService.removeItemFromCart(stockItemDto);
         assertEquals(2, cartDto.getStockItemDtoList().size());
-        assertTrue(cartDto.getStockItemDtoList().stream().anyMatch(stDto -> stDto.getItemName().equals("t-shirt") && stDto.getCartQuantity() == 1));
+        assertTrue(cartDto.getStockItemDtoList().stream().anyMatch(stDto -> stDto.getItemName().equals("test-t-shirt") && stDto.getCartQuantity() == 1));
         cart = new Cart(1, "abdulmoeedak", new HashMap<String, Integer>(){{
-            put("cup",1);
+            put("test-cup",1);
         }});
         when(cartRepository.findOneByMyShopper("abdulmoeedak")).thenReturn(cart);
         Exception exception = assertThrows(ItemNotInCartException.class, () -> mockedCartService.removeItemFromCart(stockItemDto));
         assertTrue(exception.getMessage().equals("Item not present in cart"));
     }
+
 }
