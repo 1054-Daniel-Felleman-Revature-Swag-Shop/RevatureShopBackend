bd3d76224d3d32280900df39cbe279d79bdb3cd3
pkt77 <parkerkt77@gmail.com>
2021-05-28 00:22:32 -0400

    Add MailService and send email when points added

--- accounts/src/main/java/com/revature/shop/accounts/AccountService.java
+++ accounts/src/main/java/com/revature/shop/accounts/AccountService.java
@@ -1,26 +1,46 @@
 package com.revature.shop.accounts;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.revature.shop.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StreamUtils;
 
-
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.util.Optional;
 
 @Service
 public class AccountService {
     private final AccountRepository repo;
     private final PointRepository pointsRepo;
+    private final MailService mailService;
+    private final TaskExecutor taskExecutor;
+
+    private String emailTemplate;
 
     @Autowired
-    public AccountService(AccountRepository repo, PointRepository pointsRepo) {
+    public AccountService(AccountRepository repo, PointRepository pointsRepo, MailService mailService, TaskExecutor taskExecutor) {
         this.repo = repo;
         this.pointsRepo = pointsRepo;
+        this.mailService = mailService;
+        this.taskExecutor = taskExecutor;
+
+        if (mailService != null) {
+            InputStream stream = getClass().getClassLoader().getResourceAsStream("points_email.html");
+
+            try {
+                emailTemplate = StreamUtils.copyToString(stream, Charset.defaultCharset());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Transactional
-    @HystrixCommand(fallbackMethod = "downService")
+    //@HystrixCommand(fallbackMethod = "downService")
     public boolean modPoints(int userId, PointChange change) {
         Optional<Account> optional = repo.findById(userId);
 
@@ -38,10 +58,20 @@ public class AccountService {
         repo.save(account);
         pointsRepo.save(change);
 
+        if (change.change() > 0 && mailService != null) { //Only email if points added
+            taskExecutor.execute(() -> {
+                String email = emailTemplate.replaceAll("\\{\\{NAME}}", account.getName())
+                        .replaceAll("\\{\\{POINTS}}", String.valueOf(change.change()))
+                        .replaceAll("\\{\\{REASON}}", change.cause());
+
+                mailService.sendRegistration(account.getEmail(), "RevatureShop Points", email);
+            });
+        }
+
         return true;
     }
 
-    public String downService(){
+    public String downService() {
         return "The Accounts service is currently under maintenance, please check in later";
     }
 }
