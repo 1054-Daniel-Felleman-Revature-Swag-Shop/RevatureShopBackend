66f90c00bc557f8e36d6a3c4f3b75b9bbd5d55b1
pkt77 <parkerkt77@gmail.com>
2021-05-30 03:05:24 -0400

    Add S3 credentials

--- inventory/src/main/java/com/revature/shop/services/InventoryService.java
+++ inventory/src/main/java/com/revature/shop/services/InventoryService.java
@@ -2,12 +2,12 @@ package com.revature.shop.services;
 
 import com.revature.shop.models.StockItem;
 import com.revature.shop.repositories.InventoryRepository;
-import org.eclipse.jgit.transport.AmazonS3;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
-
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -15,63 +15,60 @@ import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
 import java.io.InputStream;
-
 import java.util.List;
 
 @Service
-public class InventoryService
-{
+public class InventoryService {
     private final InventoryRepository iRep;
 
-    private final S3Client s3 = S3Client.builder().region(Region.US_EAST_2).build();
-    /*private final AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-            .withRegion(Regions.US_EAST_2)
-            .build();*/
+    private final S3Client s3;
 
-    @Autowired
     public InventoryService(InventoryRepository iRep) {
-        this.iRep = iRep;
+        this(iRep, "", "");
     }
 
-    public List<StockItem> getAllStock(){
+    @Autowired
+    public InventoryService(InventoryRepository iRep, @Value("${aws.accessKeyId}") String id, @Value("${aws.secretAccessKey}") String key) {
+        this.iRep = iRep;
+
+        s3 = S3Client.builder().region(Region.US_EAST_2).credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(id, key))).build();
+    }
+
+    public List<StockItem> getAllStock() {
         return iRep.findAll();
     }
 
-    public List<StockItem> getStockByCategory(String cat){
+    public List<StockItem> getStockByCategory(String cat) {
         return iRep.findByCategory(cat);
     }
 
-    public List<StockItem> getInStock(){
+    public List<StockItem> getInStock() {
         return iRep.findByQuantityGreaterThan(0);
     }
 
-    public List<StockItem> getOutOfStock(){
+    public List<StockItem> getOutOfStock() {
         return iRep.findByQuantityEquals(0);
     }
 
-    public Integer addToStock(StockItem sItem, MultipartFile itemImage){
+    public int addToStock(StockItem sItem, MultipartFile itemImage) {
         System.out.println("ADD TO STOCK start");
 
-        if(iRep.existsById(sItem.getId())){
+        if (iRep.existsById(sItem.getId())) {
             return -1;
         }
-        else {
-            iRep.save((sItem));
-            System.out.println("New item with id: "+sItem.getId() + " created.");
-            return sItem.getId();
 
-        }
-
+        iRep.save((sItem));
+        System.out.println("New item with id: " + sItem.getId() + " created.");
+        return sItem.getId();
     }
 
-    public Boolean updateStockItemQuantity(String name, int quantity)
-    {
-        if (iRep.findByItemName(name) != null)
-        {
+    public boolean updateStockItemQuantity(String name, int quantity) {
+        if (iRep.findByItemName(name) != null) {
             iRep.updateQuantity(name, quantity);
             return true;
         }
-        else return false;
+
+        return false;
     }
 
     public StockItem getItemByName(String itemName) {
@@ -82,26 +79,16 @@ public class InventoryService
         iRep.deleteByItemName(itemName);
     }
 
-    public boolean uploadImageForItemWithId(Integer id, MultipartFile file) {
-
-        InputStream input = null;
+    public boolean uploadImageForItemWithId(int id, MultipartFile file) {
         try {
-
             String itemId = String.valueOf(id);
+            InputStream input = file.getInputStream();
 
-            input = file.getInputStream();
-            try {
-                s3.putObject(PutObjectRequest.builder().bucket("revature-swag-shop-images").key(itemId).build(), RequestBody.fromInputStream(input, input.available()));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
+            s3.putObject(PutObjectRequest.builder().bucket("revature-swag-shop-images").key(itemId).build(), RequestBody.fromInputStream(input, input.available()));
         } catch (IOException e) {
             e.printStackTrace();
-
+            return false;
         }
         return true;
-
-
     }
 }
