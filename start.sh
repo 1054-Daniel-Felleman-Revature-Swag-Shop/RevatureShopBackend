276cc53f99201c3aec02495530ded1acf893b2d8
pkt77 <parkerkt77@gmail.com>
2021-06-01 18:28:14 -0400

    Add shell scripts to auto stop/start in jenkins

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+mkdir logs
+
+services=("eureka-server" "config" "Spring-Cloud-Gateway" "accounts" "inventory" "commerce")
+ports=(9000 8888 9001 8000 8100 8200)
+
+for ((i = 0; i < 6; i++)); do
+  service=${services[i]}
+  port=${ports[i]}
+
+  echo "Starting $service"
+
+  nohup javaw -jar "$service/target/$service-1.jar" >"logs/$service.log" &
+
+  until $(curl --output /dev/null --silent --head "http://localhost:$port"); do
+    printf '.'
+    sleep 2
+  done
+done
---
+++ stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for port in 9000 9001 8888 8000 8100 8200; do
+  curl -X POST http://localhost:$port/actuator/shutdown
+done
