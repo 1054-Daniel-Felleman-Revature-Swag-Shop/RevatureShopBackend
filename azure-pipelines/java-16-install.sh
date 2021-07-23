b10544e4debbcd835a5c2b04ffc166fb8045d0f0
Anthony <anthony.m.jarvis@gmail.com>
2021-07-23 09:38:49 -0500

    Forgot this method is interactive. Looking for another script.

--- azure-pipelines/java-16-install.sh
+++ azure-pipelines/java-16-install.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
 
-sudo add-apt-repository ppa:linuxuprising/java
-
-sudo apt update
-
-sudo apt install oracle-java16-installer
-
-sudo apt install oracle-java16-installer --no-install-recommends
-
-java --version
