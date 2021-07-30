b860e7a48ec3d84cfc0572f3d81cbcb8988ac8d3
shadekiller666 <ezioauditorerevs@gmail.com>
2021-07-29 19:55:57 -0700

    Cleaned up poms and imports for AccountController and AccountApplication.

--- accounts/src/main/java/com/revature/shop/accounts/controllers/AccountController.java
+++ accounts/src/main/java/com/revature/shop/accounts/controllers/AccountController.java
@@ -1,22 +1,24 @@
 package com.revature.shop.accounts.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.shop.accounts.models.Account;
 import com.revature.shop.accounts.models.PointHistory;
 import com.revature.shop.accounts.repositories.AccountRepository;
 import com.revature.shop.accounts.repositories.PointRepository;
 import com.revature.shop.accounts.services.AccountService;
-
-import java.awt.*;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/account")
